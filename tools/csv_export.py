d0c5ec365c1f275c88c6b8335f68411315e1b1b1
coffemakingtoaster <mh340@hdm-stuttgart.de>
2024-11-27 18:23:35 +0100

    feat(csv export): migrate to python

--- tools/csv_export.py
+++ tools/csv_export.py
@@ -1,13 +1,44 @@
-# This could also be part of the bash script...but for my (and your) sanity we will do it like this
 import sqlite3
 import csv
 import json
+import os
+import shutil
 
-con = sqlite3.connect("./tmp.db")
+TMP_DB = "./tmp.db"
+
+# merge dbs
+
+db_files = []
+
+for file in os.listdir("./in"):
+    if file.endswith(".db"):
+        db_files.append(os.path.join("./in", file))
+
+if len(db_files) == 0:
+    print("no dbs")
+    exit(1)
+
+shutil.copyfile(db_files[0], TMP_DB)
+
+con = sqlite3.connect(TMP_DB)
 
 cur = con.cursor()
 
-cur.execute("SELECT * FROM matches")
+for i in range(1, len(db_files)):
+    file = db_files[i]
+    copy_conn = sqlite3.connect(file)
+    copy_cursor = copy_conn.cursor()
+    copy_cursor.execute("SELECT * FROM matches WHERE is_relevant IS TRUE")
+    for row in copy_cursor:
+        # Construct an insert query based on the number of columns in the row
+        placeholders = ", ".join("?" for _ in row)
+        insert_query = f"INSERT OR IGNORE INTO matches VALUES ({placeholders})"
+        cur.execute(insert_query, row)
+
+    # Commit changes to Database B
+    con.commit()
+
+cur.execute("SELECT * FROM matches WHERE is_relevant IS TRUE")
 match_data_dict = {
     "matchId": "",
     "winner_BOTTOM": "",
@@ -28,7 +59,11 @@ with open("./export/export.csv", "w", newline="") as csvfile:
     w.writeheader()
     for row in cur:
         match_id, match_data, _ = row
-        match_data = json.loads(match_data)
+        try:
+            match_data = json.loads(match_data)
+        except:
+            print("Could not parse match data for one game.")
+            continue
         match_data_dict["matchId"] = match_id
         for player in match_data["info"]["participants"]:
             status = "winner" if player["win"] else "loser"
@@ -43,4 +78,5 @@ with open("./export/export.csv", "w", newline="") as csvfile:
         w.writerow(match_data_dict)
         cnt += 1
 
+os.remove(TMP_DB)
 print(f"Exported {cnt} matches!")
